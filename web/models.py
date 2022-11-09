2d7d8ea8f092e4ae63a58855e7ca14559b3c77a6
AliJ752 <alistair.jones3@nhs.net>
2022-11-09 14:51:32 +0000

    Patient identifier, variant information and evidence code models

--- web/models.py
+++ web/models.py
@@ -6,11 +6,26 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class PID(models.Model):
-    Name = models.CharField(max_length=100, null=False)
-    Age = models.IntegerField(max=130, null=False)
-    Relatives = models.BooleanField(null=True)
+class patient_identifier(models.Model):
+    forename = models.CharField(max_length=100, null=False)
+    surname = models.IntegerField(max=130, null=False)
+    age = models.BooleanField(null=True)
+    proband = models.CharField(max_length=100, null=False)
+    affected_relatives = models.CharField(max_length=100, null=False)
+    tumor = models.CharField(max_length=100, null=False)
+    sequencer = models.CharField(max_length=100, null=False)
+    g_nomenclature = models.CharField(max_length=100, null=False)
+    pathogenecity_code = models.CharField(max_length=100, null=False)
+    evidence_codes = models.CharField(max_length=100)
 
+class variant_information(models.Model):
+    g_nomenclature = models.ForeignKey(patient_identifier, db_column='g_nomenclature', on_delete=models.CASCADE)
+    variant_protein = models.CharField(max_length=100, null=False)
+    c_nomenclature = models.CharField(max_length=100, null=False)
+    
+class evidence_codes(models.Model):
+    code_ID = models.ForeignKey(patient_identifier, db_column='evidence_codes', on_delete=models.CASCADE)
+    evidence_codes = models.CharField(max_length=100, null=False)
 
  def publish(self):
     self.published_date = timezone.now()
