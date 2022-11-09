9a41f3d26485b10439ab8a7672841ef340fd62a1
Arnold E.C <eca1g14@soton.ac.uk>
2022-11-09 14:27:24 +0000

    Added Sequencer Choices

--- web/models.py
+++ web/models.py
@@ -5,14 +5,30 @@ from django.db import models
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django import forms
+  
+
+  
+# creating a form 
+class GeeksForm(forms.Form):
+    geeks_field = forms.ChoiceField(choices = GEEKS_CHOICES)
 
 class PID(models.Model):
     Name = models.CharField(max_length=100, null=False)
     Age = models.IntegerField(max=130, null=False)
     Relatives = models.BooleanField(null=True)
 
+# iterable
+SequencerChoices = [
+    'MiSeq',
+    'HiSeq',
+    'NextSeq',
+    ]
 
- def publish(self):
+class TableSequencer(models.Model):
+    Sequencer = models.CharField(choices=SequencerChoices)
+
+def publish(self):
     self.published_date = timezone.now()
     self.save()
 
