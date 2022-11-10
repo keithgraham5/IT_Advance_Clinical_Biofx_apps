c203644c1c52a3e9bb71cb193aeac150d14a5289
keith.graham5 <keith.graham5@nhs.net>
2022-11-10 15:06:30 +0000

    Import, read and convert variant file into a pandas dataframe. Split full name into surname and forname columns

--- seed_data.py
+++ seed_data.py
@@ -2,10 +2,10 @@ import pandas as pd
 from django.core.management.base import BaseCommand, CommandError
 
 
-python manage.py seed_data
---file "/home/keith/Data/Omimgenemap2.txt"
---type 'omin_gene_map'
---data_version 'version-1.0'
+# python manage.py seed_data
+# --file "/home/keith/Data/Omimgenemap2.txt"
+# --type 'omin_gene_map'
+# --data_version 'version-1.0'
 class Command(BaseCommand):
     help = "Seed data for a given source"
 
@@ -28,18 +28,108 @@ class Command(BaseCommand):
             _process_brca1_variants_list(options["file"])
 
 
-variant_list_file = '/home/keith/Data/IT_Adv_Biofx_app/BRCA1 spreadsheet(2).txt
-def _process_brca1_variant_list(variant_list_file):
-    # Read in the variant list file
-    variant_dataframe = pd.read_csv(variant_list_file, on_bad_lines='skip',
-                                    sep='\t', lineterminator='\n', header=None )
-    # Create a pandas data frame
-    variant_dataframe = pd.DataFrame(variant_dataframe)
-    # Replace the null values with specified values
-    variant_dataframe.fillna('', inplace=True)
-    # Store contents of variant_data
-    new_header = variant_dataframe.iloc[0]
-    variant_dataframe = variant_dataframe[1:]
-    variant_dataframe.columns = new_header
-    print(variant_dataframe)
+variant_list_file = '/home/keith/Data/IT_Adv_Biofx_app/BRCA1 spreadsheet(2).txt'
+
+# Read in the variant list file
+variant_dataframe = pd.read_csv(variant_list_file, on_bad_lines='skip',
+                                sep='\t', lineterminator='\n')
+# Create a pandas data frame
+variant_dataframe = pd.DataFrame(variant_dataframe)
+# Replace the null values with specified values
+variant_dataframe.fillna('', inplace=True)
+variant_dataframe.drop('Unnamed: 10', axis=1, inplace=True)
+# Change header names on dataframe
+variant_dataframe.columns = ['name','age','proband','affected_relatives',
+                             'Stage','tumor','sequencer','c_nomenclature',
+                             'variant_protein','g_nomenclature',
+                             'pathogenecity_code', 'evidence_codes']
+# Create a list of forenames
+forename_list = []
+# Create a list of surnames
+surname_list = []
+# Get full_name from name column
+for i in variant_dataframe.iterrows():
+    # Get full name
+    full_name = i[1]["name"]
+    # Get forename from surname
+    forename = full_name.strip().split(" ")[0]
+    # Append forename to forename_list
+    forename_list.append(forename)
+    # Get surname from surname
+    surname = full_name.strip().split(" ")[1]
+    # Append surname to surname list
+    surname_list.append(surname)
+# Add forename column to dataframe populate with forename_list
+variant_dataframe = variant_dataframe.assign(forename=forename_list)
+# Add surname column to the dataframe populate with surname_list
+variant_dataframe = variant_dataframe.assign(surname=surname_list)
+
+# # Iterate over each row in the dataframe
+# for row in variant_dataframe:
+#     variant = variant_dataframe[]
+# # Add the contents of each for to the database
+
+
+
+
+# Sanity check !!!
+# print(full_name)
+# print(forename)
+# print(surname)
+# print(forename_list)
+# print(surname_list)
+print(variant_dataframe)
+
+
+
+    # # Get forename from fullname
+    # forename = full_name[1]
+
+    # # Get surname from fullname
+    # surname = full_name[2]
+    # # Using 'surname' as the column name equate its to the value surname
+    # variant_dataframe['surname'] = surname
+
+
+
+#     print(i)
+#     print(i[1])
+#     print(type(i[1]))
+#     print(i[1]["name"])
+#     full_name = str(i[1]["name"]).split(' ')[0]
+#     print(full_name)
+#     # Get forename from fullname
+#     forename = full_name[1]
+#     # Using 'forename' as the column name equate its to the value forename
+#     i['forename'] = forename
+#     # Get surname from fullname
+#     surname = full_name[2]
+#     # Using 'surname' as the column name equate its to the value surname
+#     ['surname'] = surname
+# # Insert new column (forename)
+
+# Insert new column (surname)
+# print(variant_dataframe)
+
+
+# Change header names on dataframe
+# variant_dataframe.columns = ['name','age','proband','affected_relatives',
+#                              'Stage','tumor','sequencer','c_nomenclature',
+#                              'variant_protein','g_nomenclature',
+#                              'pathogenecity_code', 'evidence_codes',
+#                              'forename','surename']
+
+# Get full_name from name column
+# full_name = variant_dataframe['name'].str.split(' ')[0]
+# Get forname from fullname
+# forename = full_name[1]
+# Get surname from fullname
+# surname = full_name[2]
+# Move surname to surname column
+# Move surname to forname column
+
+# print(variant_dataframe)
+
+
+
 
