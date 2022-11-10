f501aeafc48eddfc12eec75f30fbc4f2c45cb025
Arnold E.C <eca1g14@soton.ac.uk>
2022-11-10 15:05:05 +0000

    Changes made with Ali

--- web/views.py
+++ web/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import patient_identifier
+from .models import variant_information
 
 # Create your views here.
 def variant_list(request):
     patients = patient_identifier.objects.all()
-    return render(request, 'web/variant_list.html', {'patients':patients})
+    variants = variant_information.objects.all()
+    return render(request, 'web/variant_list.html', {'patients':patients, 'variants':variants})
 
