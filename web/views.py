c2a938976d28bef236df26a276a1742fac620012
AliJ752 <alistair.jones3@nhs.net>
2022-11-10 11:40:47 +0000

    Added a post all view to the application views

--- web/views.py
+++ web/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import patient_identifier
 
 # Create your views here.
 def variant_list(request):
-    return render(request, 'web/variant_list.html', {})
+    patients = patient_identifier.objects.all()
+    return render(request, 'web/variant_list.html', {'patients':patients})
+
