0e9235d8a4e09128c63c7cbd7a13d29194d2d288
AliJ752 <alistair.jones3@nhs.net>
2022-11-11 07:45:26 +0000

    Search bar working for all fields which brings up the returned searches on the search.html

--- web/views.py
+++ web/views.py
@@ -8,3 +8,22 @@ def variant_list(request):
     variants = variant_information.objects.all()
     return render(request, 'web/variant_list.html', {'patients':patients, 'variants':variants})
 
+def searchpage(request):
+    srh = request.GET['query']
+    patients = \
+    patient_identifier.objects.filter(forename__icontains=srh) | \
+    patient_identifier.objects.filter(surname__icontains=srh) | \
+    patient_identifier.objects.filter(age__icontains=srh) | \
+    patient_identifier.objects.filter(proband__icontains=srh) | \
+    patient_identifier.objects.filter(affected_relatives__icontains=srh) | \
+    patient_identifier.objects.filter(tumor__icontains=srh) | \
+    patient_identifier.objects.filter(Stage__icontains=srh) | \
+    patient_identifier.objects.filter(sequencer__icontains=srh) | \
+    patient_identifier.objects.filter(forename__icontains=srh) | \
+    patient_identifier.objects.filter(pathogenecity_code__icontains=srh) | \
+    patient_identifier.objects.filter(evidence_codes__icontains=srh) | \
+    patient_identifier.objects.filter(g_nomenclature__g_nomenclature__icontains=srh) | \
+    patient_identifier.objects.filter(g_nomenclature__c_nomenclature__icontains=srh) | \
+    patient_identifier.objects.filter(g_nomenclature__variant_protein__icontains=srh)
+    params = {'patients':patients,'search': srh}
+    return render(request, 'web/search_page.html', params)
